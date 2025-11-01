de2c627278980540d64eaf93d1ac387c07220560
Gizem Demirci <gizdemirci91@gmail.com>
2025-11-01 19:12:16 +0300

    Add .gitignore and remove node_modules from tracking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import { lazy } from 'react';
+
+import SharedLayout from './SharedLayout/SharedLayout';
+import Notify from './common/Notify/Notify';
+
+import RestrictedLoginPage from 'pages/LoginPage/RestrictedLoginPage';
+import RestrictedRegisterPage from 'pages/RegisterPage/RestrictedRegisterPage';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+
+const HomePage = lazy(() => import('pages/HomePage/RestrictedHomePage'));
+const StatisticsPage = lazy(() =>
+  import('pages/StatisticsPage/RestrictedStatisticsPage')
+);
+const CurrencyPage = lazy(() =>
+  import('pages/CurrencyPage/RestrictedCurrencyPage')
+);
+
+const App = () => {
+  const isOnMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<RestrictedLoginPage />} />
+        <Route path="/register" element={<RestrictedRegisterPage />} />
+
+        <Route path="/dashboard" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="statistics" element={<StatisticsPage />} />
+          {isOnMobile && <Route path="currency" element={<CurrencyPage />} />}
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+
+      <Notify />
+    </>
   );
 };
+
+export default App;
