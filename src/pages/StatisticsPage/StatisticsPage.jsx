3cc1a010de672c1fdbb89728ecb26eed248edd38
Aykut <ayktshnbs@gmail.com>
2025-11-02 19:03:06 +0300

    Added images and pages

---
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -0,0 +1,22 @@
+import styles from './StatisticsPage.module.css';
+import StatisticsDashboard from '../../components/StatisticsDashboard/StatisticsDashboard';
+import StatisticsTable from '../../components/StatisticsTable/StatisticsTable';
+import StatisticsChart from 'components/StatisticsChart/StatisticsChart';
+
+const StatisticsPage = () => {
+  return (
+    <div className={styles.statisticsPage}>
+      <div className={styles.titleAndChart}>
+        <h1 className={styles.title}>Statistics</h1>
+        <StatisticsChart />
+      </div>
+
+      <div className={styles.dashboardAndTable}>
+        <StatisticsDashboard />
+        <StatisticsTable />
+      </div>
+    </div>
+  );
+};
+
+export default StatisticsPage;
